23902233f673a2a85026d00d4d6a1f9c30f1bf1b
shamshad0786 <shamshad1840@gmail.com>
2017-02-19 14:33:26 +0530

    ALL

--- loginid.php
+++ loginid.php
@@ -5,12 +5,12 @@ $hostname = "localhost";
 $password = "";
 $usertable = "User_info";
 $yourfield = "Uid";
-$height=$_POST['height'];
-$weight=$_POST['weight'];
+$height=$_GET['height'];
+$weight=$_GET['weight'];
     $connection = mysql_connect($hostname, $username, $password,$dbname) OR DIE ("Unable to 
 connect to database! Please try again later.");
     mysql_select_db($dbname);
-$query = "SELECT * FROM $usertable WHERE $yourfield = '".$_POST['email']."'";
+$query = "SELECT * FROM $usertable WHERE $yourfield = '".$_GET['email']."'";
     $result = mysql_query($query);
     if ($result) {
         while($row = mysql_fetch_array($result)) 
